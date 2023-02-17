84028399aeef95652244a0defee8b3c62fd5177f
Kyle Naylor <terminal109@protonmail.com>
2023-02-16 19:21:39 -0500

    added backgrounds

---
+++ screenlayouts/none.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output HDMI-A-0 --off --output HDMI-A-1 --off --output DVI-D-0 --off
---
+++ screenlayouts/one.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output HDMI-A-0 --primary --mode 1920x1080 --pos 1920x46 --rotate normal --output HDMI-A-1 --off --output DVI-D-0 --off
